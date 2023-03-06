f9f4f7eb0e6768cb8e3d0355d4f50528b2a31f18
Bui Khoi <67303085+icyj1003@users.noreply.github.com>
2023-03-06 21:44:57 +0700

    add results

--- results.py
+++ results.py
@@ -1,7 +1,19 @@
 from river.metrics import Accuracy, MacroF1
 from utils import load_object
+import pandas as pd
 
-for a in ['lstm', 'gru', 'cnn']:
+model = ['lstm', 'gru', 'cnn']
+acc_list = []
+f1_list = []
+
+for a in model:
     acc = load_object(f'./model/{a}/acc.pkl')
     f1 = load_object(f'./model/{a}/f1.pkl')
-    print(f"{a} - Accuracy: {acc.get()}, F1Macro: {f1.get()}")
+    c1 = acc.get()
+    c2 = f1.get()
+    acc_list.append(c1)
+    f1_list.append(c2)
+    print(f"{a} - Accuracy: {c1}, F1Macro: {c2}")
+
+df = pd.DataFrame(data={'model': model, 'acc': acc_list, 'f1-macro': f1_list})
+df.to_csv('./resources/results.csv', index=False)
